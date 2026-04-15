999f9f838279cdd76fb670b0cb063db68e1f65a2
wanpengfei-git <wanpengfei91@163.com>
2026-04-15 08:04:47 +0000

    style: auto-fix clang-format issues

--- be/src/exec/pipeline/pipeline_builder.cpp
+++ be/src/exec/pipeline/pipeline_builder.cpp
@@ -504,8 +504,7 @@ OpFactories PipelineBuilderContext::maybe_interpolate_debug_ops(RuntimeState* st
     auto action_opt = runtime_state()->debug_action_mgr().get_debug_action(plan_node_id);
     if (action_opt.has_value() && action_opt.value().is_pipeline_break_action()) {
         auto* pred_source_op = source_operator(pred_operators);
-        auto wait_context_factory =
-                std::make_shared<WaitContextFactory>(action_opt->value, action_opt->action);
+        auto wait_context_factory = std::make_shared<WaitContextFactory>(action_opt->value, action_opt->action);
         auto wait_sink =
                 std::make_shared<WaitOperatorSinkFactory>(next_operator_id(), plan_node_id, wait_context_factory);
 
